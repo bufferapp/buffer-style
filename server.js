26b5d6b350c3b01445398bea743d9eb0a326b045
Sunil Sadasivan <sunil@bufferapp.com>
2015-08-19 18:51:36 -0400

    Initial commit for Buffer Style and Guide base

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+http = require('http');
+app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.use(express.static(__dirname + '/public'))
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Style Guide at http://%s:%s', host, port);
+});
