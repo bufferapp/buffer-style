2340546a062d25ff9e1bac48548886a374486dbe
Joel Gascoigne <me@joel.is>
2015-08-21 08:48:04 -0400

    a little work on the plane on the style guide - flexbox and layout work

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,15 @@ var React = require('react');
 var BufferAvatar = require('./BufferAvatar');
 
 React.render(
-  <BufferAvatar type={'small'} imgsrc={'https://buffer.com/images/team/sunil.png'} />, document.getElementById('avatarsmall')
+  <BufferAvatar type={'small'} imgsrc={'https://local.buffer.com/images/team/sunil.png'} />, document.getElementById('avatarsmall')
 );
 
 React.render(
-  <BufferAvatar type={'medium'} imgsrc={'https://buffer.com/images/team/sunil.png'} />, document.getElementById('avatarmedium')
+  <BufferAvatar type={'medium'} imgsrc={'https://local.buffer.com/images/team/sunil.png'} />, document.getElementById('avatarmedium')
 );
 
 React.render(
-  <BufferAvatar type={'large'} imgsrc={'https://buffer.com/images/team/sunil.png'} />, document.getElementById('avatarlarge')
+  <BufferAvatar type={'large'} imgsrc={'https://local.buffer.com/images/team/sunil.png'} />, document.getElementById('avatarlarge')
 );
 
 
